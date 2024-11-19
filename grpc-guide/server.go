a5cc4a0d6a63e1e1eebb3b5918a87b279cb33ca0
ZhongLang <158394205+zhonglangj@users.noreply.github.com>
2024-11-19 09:55:29 +0900

    chat server

---
+++ grpc-guide/server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen on port 9000: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to server gRPC server over port 9000: %v", err)
+	}
+}
